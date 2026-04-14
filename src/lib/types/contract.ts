a2848394a87489a90fbff4dbed891fa5d4d84020
(주)피드백1 <feedback1@jupideubaeg1ui-iMac.local>
2026-04-14 11:49:59 +0900

    피라미드 구조

--- src/lib/types/contract.ts
+++ src/lib/types/contract.ts
@@ -55,7 +55,6 @@ export interface Contract {
 
 export interface ContractInsert {
   contract_code: string;
-  sequence_no?: number | null;
   rental_request_no?: string | null;
   memo?: string | null;
   customer_id: string;
